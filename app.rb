a69bc1f04cdfc6cc6c57663c6ccd3ac6500d3a08
Amon <amonmoce@yahoo.fr>
2014-12-30 22:07:26 +0800

    views okay

--- app.rb
+++ app.rb
@@ -1,10 +1,28 @@
 require 'sinatra/base'
+require 'sinatra/flash'
 require 'haml'
 
 class ClitApp < Sinatra::Base
-
+  register Sinatra::Flash
+  
   get '/' do
       haml :home
+  end
+
+  get '/register' do
+    haml :register
+  end
+
+  get '/start' do
+    haml :start
+  end
+
+  get '/list' do
+    haml :list
+  end
+
+  get '/translated' do
+    haml :translated
   end
 
 end
