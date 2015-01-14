d9759f285cb8fb934cb0b6714a9432da6ffe38fd
Amon <amonmoce@yahoo.fr>
2015-01-14 18:13:08 +0800

    pdfdata table attributes added

--- model/pdfdata.rb
+++ model/pdfdata.rb
@@ -4,6 +4,13 @@ class Pdfdata < AWS::Record::HashModel
 
   string_attr  "pdfdata"
   string_attr  "classid"
+  string_attr  "english"
+  string_attr  "mandarin_tr"
+  string_attr  "spanish"
+  string_attr  "ukrainian"
+  string_attr  "vietnamese"
+  string_attr  "indonesian"
+  string_attr  "malaysian"
   timestamps
 
 end
